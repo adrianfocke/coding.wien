838434447d954e8a2c4917838d3c4d254f9740f9
Focke Adrian <afocke@pm.me>
2024-09-18 16:56:16 +0200

    remove unused import

--- components/Form/template.ts
+++
@@ -1,8 +0,0 @@
-import type { Template } from "tinacms";
-import { TitleField, WidthField } from "../../tina/fields";
-
-export const FormTemplate: Template = {
-  name: "Form",
-  label: "Form",
-  fields: [TitleField, WidthField as any],
-};
