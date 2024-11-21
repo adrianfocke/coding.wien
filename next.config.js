53265dcaa2efb68cdaa5b1854422f63760069d62
Focke Adrian <afocke@pm.me>
2024-11-21 12:52:41 +0100

    add tina assets config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  images: {
+    domains: ["assets.tina.io"],
+  },
   async rewrites() {
     return [
       {
@@ -13,4 +16,4 @@ module.exports = {
       },
     ];
   },
-}
+};
