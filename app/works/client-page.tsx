9d8897c4c2e8ca1c573a5f95db1c44431cfb0c4f
Focke Adrian <afocke@pm.me>
2024-12-01 21:36:36 +0100

    add margin

--- app/works/client-page.tsx
+++ app/works/client-page.tsx
@@ -1,12 +1,13 @@
 "use client";
+import { Container } from "@radix-ui/themes";
 import Grid, { GridVariant } from "../../components/Grid/Grid";
 
 export default function ClientPage() {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
 
   return (
-    <div>
+    <Container mx="2">
       <Grid variant={GridVariant["Work-List"]}></Grid>
-    </div>
+    </Container>
   );
 }
