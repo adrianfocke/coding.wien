884a14b73cc1000087df90399b26df2e5e93382b
Focke Adrian <afocke@pm.me>
2024-12-01 20:41:58 +0100

    add remote patterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,27 @@
 
 module.exports = {
   images: {
-    domains: ["assets.tina.io"], // Ensure this array is not empty or undefined
+    domains: ["assets.tina.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+        port: "",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "**.vercel.com",
+        port: "",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "content.tinajs.io",
+        port: "",
+        search: "",
+      },
+    ],
   },
   async rewrites() {
     return [
